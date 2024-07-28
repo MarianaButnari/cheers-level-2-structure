70917ffe174acdbcd1e41a36d55183cfae020a8a
Mariana <mariana.butnari89@gmail.com>
2024-07-28 13:25:31 +0300

    enh(ui): lazy loading cocktails and cocktail-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,18 @@
 import {Routes} from '@angular/router';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 import {cocktailDetailsResolver} from './shared/utils/cocktail-details.resolver';
-import {CocktailListComponent} from './components/cocktail-list/cocktail-list.component';
-import {CocktailDetailsComponent} from './components/cocktail-details/cocktail-details.component';
 
 export const routes: Routes = [
   {
     path: 'cocktails',
-    component: CocktailListComponent
-    // loadComponent: () =>
-    //   import('./components/cocktail-list/cocktail-list.component').then(
-    //     (m) => m.CocktailListComponent,
-    //   ),
+    loadComponent: () =>
+      import('./components/cocktail-list/cocktail-list.component').then(
+        (m) => m.CocktailListComponent,
+      ),
   },
   {
     path: 'cocktails/:cocktailId',
-    component: CocktailDetailsComponent,
-    // loadComponent: () => import('./components/cocktail-details/cocktail-details.component').then(m => m.CocktailDetailsComponent),
+    loadComponent: () => import('./components/cocktail-details/cocktail-details.component').then(m => m.CocktailDetailsComponent),
     resolve: {
       cocktail: cocktailDetailsResolver
     }
