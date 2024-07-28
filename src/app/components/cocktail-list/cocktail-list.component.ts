df6056a807776b6378f68b6da19cfbb18e5e2a74
Mariana <mariana.butnari89@gmail.com>
2024-07-28 22:11:30 +0300

    enh(ui): unsubscribe to avoid memory leaks

--- src/app/components/cocktail-list/cocktail-list.component.ts
+++ src/app/components/cocktail-list/cocktail-list.component.ts
@@ -1,10 +1,11 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {debounceTime} from 'rxjs';
 import {CocktailsService} from '../../shared/services/cocktails.service';
 import {Cocktail} from '../../shared/interfaces/cocktail';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {CocktailItemComponent} from '../cocktail-item/cocktail-item.component';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-cocktail-list',
@@ -18,20 +19,28 @@ export class CocktailListComponent implements OnInit {
   protected searchInput = new FormControl<string | null>(null);
   protected filtered: Cocktail[];
   private cocktails: Cocktail[];
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     this.initData();
-    this.searchInput.valueChanges.pipe(debounceTime(500)).subscribe(value => {
-      value ? this.filtered = this.cocktails.filter(cocktail => cocktail.name.toLowerCase().includes(value)) :
-        this.filtered = this.cocktails
-    });
+    this.searchInput.valueChanges
+      .pipe(
+        debounceTime(500),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((value: string | null) => {
+        value ? this.filtered = this.cocktails.filter(cocktail => cocktail.name.toLowerCase().includes(value)) :
+          this.filtered = this.cocktails
+      });
   }
 
   private initData() {
-    this.cocktailsService.getAllCocktails().subscribe((response: Cocktail[]) => {
-      this.cocktails = response;
-      this.filtered = this.cocktails;
-    })
+    this.cocktailsService.getAllCocktails()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((response: Cocktail[]) => {
+        this.cocktails = response;
+        this.filtered = this.cocktails;
+      });
   }
 
 }
