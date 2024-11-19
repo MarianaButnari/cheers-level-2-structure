7441e4f337e84960deac9e5c3af9fa429c6250c6
Mariana <mariana.butnari89@gmail.com>
2024-11-19 16:32:28 +0200

    fix cocktail-details spec and add some more test on app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,28 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
 describe(`AppComponent`, () => {
-    // ?this is to check that our tools work as expected
-  it('should pass sanity test', () => {
-    expect(true).toBeTruthy()
+  // ?this is to check that our tools work as expected
+  // it('should pass sanity test', () => {
+  //   expect(true).toBeTruthy()
+  // });
+
+  // !real stuff going here :D
+  beforeEach(() => {
+    // !crucial thing is this TestBed, it creates a testing module
+    TestBed.configureTestingModule({
+      // declarations: [AppComponent],
+      // !since AppComponent is a standalone one it wont work with declarations
+      imports: [AppComponent],
+      providers: [provideHttpClient(), provideRouter([])],
+    }).compileComponents(); // !this method inlines our HTML and CSS, however Angular does this for us
+  });
+
+  it('should create the App', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
--- src/app/components/cocktail-details/cocktail-details.component.spec.ts
+++ src/app/components/cocktail-details/cocktail-details.component.spec.ts
@@ -1,48 +1,53 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CocktailDetailsComponent } from './cocktail-details.component';
-import { of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CocktailDetailsComponent', () => {
   let component: CocktailDetailsComponent;
   let fixture: ComponentFixture<CocktailDetailsComponent>;
-  let mockActivatedRoute: any;
-// TODO this test fail , need to fix it
+
   beforeEach(waitForAsync(() => {
-    // Mock ActivatedRoute
-    mockActivatedRoute = {
+    const mockActivatedRoute = {
       data: of({
-        id: '17141',
-        name: 'Smut',
-        isAlcoholic: 'Alcoholic',
-        imageUrl:
-          'https://www.thecocktaildb.com/images/media/drink/rx8k8e1504365812.jpg',
-        instructions: 'Throw it all together and serve real cold.',
-        ingredients: [
-          'Red wine',
-          'Peach schnapps',
-          'Pepsi Cola',
-          'Orange juice',
-        ],
+        cocktail: {
+          id: '17141',
+          name: 'Smut',
+          isAlcoholic: 'Alcoholic',
+          imageUrl:
+            'https://www.thecocktaildb.com/images/media/drink/rx8k8e1504365812.jpg',
+          instructions: 'Throw it all together and serve real cold.',
+          ingredients: [
+            'Red wine',
+            'Peach schnapps',
+            'Pepsi Cola',
+            'Orange juice',
+          ],
+        },
       }),
     };
 
     TestBed.configureTestingModule({
       imports: [CocktailDetailsComponent],
-      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+      ],
     }).compileComponents();
-
     fixture = TestBed.createComponent(CocktailDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should get correct data from the route data', waitForAsync(async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
 
-  it('should get correct data from the route data', (done) => {
     component.cocktail$.subscribe((cocktail) => {
       expect(cocktail).toEqual({
         id: '17141',
@@ -58,7 +63,6 @@ describe('CocktailDetailsComponent', () => {
           'Orange juice',
         ],
       });
-      done();
     });
-  });
+  }));
 });
